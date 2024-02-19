187c9c66cfdff5865fd3b43aba8dedfb20f0b8da
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-02-19 23:02:28 +0700

    style: styling header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,36 @@
+"use client";
 import React from 'react';
+import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
+import { useDispatch, useSelector } from 'react-redux';
+import { setViews } from '@/lib/action/viewsAction';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const pageViews = useSelector((state: any) => state.pages.pageViews);
+
+  const handleBack = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const value = e.currentTarget.value;
+    dispatch(setViews(value));
+  };
+
+
+  const HeaderDisplay = () => {
+    return (
+      <>
+        <button className='m-auto' value="home" onClick={(e) => handleBack(e)} type='button'>
+          {pageViews === "home" ? <></> : <ArrowBackIosRoundedIcon />}
+        </button>
+        <h1 className='m-auto font-bold grid-cols-2'>
+          {pageViews === "bag" || pageViews === "inventory" ? "BAG" : "HOME"}
+        </h1>
+      </>
+    );
+  };
+
   return (
-    <div>Header</div>
+    <header className='p-3 mx-auto grid grid-cols-3 w-full max-w-md mb-3 sticky top-0 h-16 shadow-[0_0_10px_rgba(0,0,0,0.3)]'>
+      <HeaderDisplay />
+    </header>
   );
 };
 
